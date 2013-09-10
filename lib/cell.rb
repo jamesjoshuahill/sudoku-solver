455b29984fe376ef39647d6597788afffc06fa7e
Josh Hill <jamesjoshuahill@gmail.com>
2013-09-10 16:41:09 +0100

    Finished draft of cell spec

--- lib/cell.rb
+++ lib/cell.rb
@@ -13,4 +13,16 @@ class Cell
   def neighbours
     @neighbours
   end
+
+  def values_of_neighbours
+    @neighbours.map(&:value)
+  end
+
+  def possible_values
+    (1..9).to_a - values_of_neighbours
+  end
+
+  def solve
+    @value = possible_values.first if possible_values.count == 1
+  end
 end
--- spec/cell_spec.rb
+++ spec/cell_spec.rb
@@ -2,9 +2,9 @@ require 'cell'
 
 describe Cell do
   let(:neighbours) { (1..28).to_a }
+  let(:cell) { Cell.new(0, neighbours) }
 
   it 'should have a value' do
-    cell = Cell.new(0, neighbours)
     expect(cell.value).to eq 0
     cell = Cell.new(1, neighbours)
     expect(cell.value).to eq 1
@@ -16,13 +16,49 @@ describe Cell do
   end
 
   it 'should know if it has not been filled out' do
-    cell = Cell.new(0, neighbours)
     expect(cell).not_to be_filled_out
   end
 
   it 'should have 28 neighbours' do
-    cell = Cell.new(0, neighbours)
     expect(cell.neighbours).to be neighbours
     expect(cell.neighbours.count).to eq 28
+  end
+
+  it 'should know the values of it\'s neighbours' do
+    neighbour = double :Cell, value: 1
+    neighbours = [neighbour] * 28
+    cell = Cell.new(0, neighbours)
+    expect(cell.values_of_neighbours).to eq([1] * 28)
+
+    neighbour = double :Cell, value: 5
+    neighbours = [neighbour] * 28
+    cell = Cell.new(0, neighbours)
+    expect(cell.values_of_neighbours).to eq([5] * 28)
+  end
+
+  context 'when it has not been filled out' do
+    it 'should know which values are possible' do
+      expect(cell).to receive(:values_of_neighbours).and_return [1, 2, 3, 4]
+      expect(cell.possible_values).to eq [5, 6, 7, 8, 9]
+
+      expect(cell).to receive(:values_of_neighbours).and_return [5, 6, 7, 8, 9]
+      expect(cell.possible_values).to eq [1, 2, 3, 4]
+    end
+
+    it 'should solve itself if there is only one possible value' do
+      expect(cell).to receive(:possible_values).twice.and_return [5]
+      cell.solve
+      expect(cell.value).to eq 5
+
+      expect(cell).to receive(:possible_values).twice.and_return [3]
+      cell.solve
+      expect(cell.value).to eq 3
+    end
+
+    it 'should not solve itself if there is more than one possible value' do
+      expect(cell).to receive(:possible_values).and_return [1, 2, 3]
+      cell.solve
+      expect(cell).not_to be_filled_out
+    end
   end
 end
